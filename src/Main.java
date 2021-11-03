dcb5996a52bdc6abc21928f22f8ce9f6d6a4927d
GLEB KARAPETYAN <carapetyangleb@yandex.ru>
2021-11-03 17:49:09 +0200

    ad

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main<miles> {
+    public static void main(String[] args) {
+        int priceBilet = 1000;
+        int bonusMiles = 20;
+        int miles = priceBilet / bonusMiles;
+        System.out.println(miles);
+    }
+}
